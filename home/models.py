2cac47478b39db702221476aa5a2884ac7438872
jwogrady <john@status26.com>
2022-12-27 20:25:12 -0600

    add standard page

--- home/models.py
+++ home/models.py
@@ -1,7 +1,37 @@
 from django.db import models
-
+from wagtail.admin.panels import FieldPanel
+from wagtail.fields import StreamField
 from wagtail.models import Page
+
+from .blocks import BaseStreamBlock
 
 
 class HomePage(Page):
     pass
+
+
+class StandardPage(Page):
+    """
+    A generic content page. On this demo site we use it for an about page but
+    it could be used for any type of page content that only needs a title,
+    image, introduction and body field
+    """
+
+    introduction = models.TextField(
+        help_text="Text to describe the page", blank=True)
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        help_text="Landscape mode only; horizontal width between 1000px and 3000px.",
+    )
+    body = StreamField(
+        BaseStreamBlock(), verbose_name="Page body", blank=True, use_json_field=True
+    )
+    content_panels = Page.content_panels + [
+        FieldPanel("introduction"),
+        FieldPanel("body"),
+        FieldPanel("image"),
+    ]
